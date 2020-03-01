d558b57efa29d828c1d604faaabd657fce176e91
mayankdevparihar1988 <mayank.dev.parihar1988@gmail.com>
2020-03-01 14:21:23 +0100

    adding the commit name

---
+++ constants/index.js
@@ -0,0 +1,13 @@
+module.exports = {
+    defaultServerRespose:{
+        status: 400,
+        message: '',
+        body: {}
+    },
+    productMessage: {
+
+        PRODUCT_CREATED:"Product created Successfully!!"
+
+
+    }
+}
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,18 +1,24 @@
 const express = require('express');
 const productService = require('../service/productService');
 const router = express.Router();
+const constants = require('../constants');
 
 router.post('/', (req,res)=>{
+
+    let response = {...constants.defaultServerRespose};
     productService.createProduct(req.body).then((data)=>{
 
-        res.status(201).send(`Product created Successfully!! data == ${data}`);
+        response.status = 201;
+        response.message= constants.productMessage.PRODUCT_CREATED;
+        response.body= data;
+        res.status(201).send(response);
 
 
     }).catch((error)=> {
 
-         console.log('error -- ', error);
-
-        throw new Error(error); 
+        response.message = error.message;
+        res.send(response);
+       
     });
     
 });
